a12c577f9fc0d7b2e96c78a4442d5011c44b6b5b
Unknown <justineagnelet@gmail.com>
2018-04-23 09:39:55 +0200

    HTML Jeu Solo à Confirmer

--- php/solo_game.php
+++ php/solo_game.php
@@ -1,3 +1,49 @@
 <?php
 	header("Content-type: text/html; charset: UTF-8");
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="" content="">
+    <title></title>
+    <meta name="" content="">
+    <link rel="" href="">
+
+</head>
+
+<body>
+    <?php include("./main_header.php")?>
+    
+    <main>
+
+        <section>
+            
+            <div id="photoProfil"></div>
+            
+            <div id="score"></div>
+
+            <input type="button" value="?" id="tutoGameButton" class="button">
+            
+            <p>N°1</p>
+            
+            <p>Phrase à compléter</p>
+            
+            <input type="button" value="Réponse n°1" id="reponse1Button" class="button">
+            
+            <div id="timer"></div>
+            
+            <input type="button" value="Réponse n°2" id="reponse2Button" class="button">
+            
+            
+        </section>
+
+    </main>
+
+    <?php include("./main_footer.php")?>
+</body>
+
+</html>
