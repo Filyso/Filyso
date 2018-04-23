1ac4c927133526cbf5b444ae63305bc170a17d4e
Unknown <vscavinner@gmail.com>
2018-04-23 11:06:17 +0200

    calculScore v2
    Ajout de la fonction Math.round pour arrondir à 0.05

--- javascript/score.js
+++ javascript/score.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", initialiser);
 
 function initialiser(evt) {
 
-    var questions  = document.getElementsByTagName(); <--???????-->
+    var questions  = document.getElementsByTagName();
     
     var derniereQuestion = document.getElementById("");
+    
+    var scoreGeneral = 0 ;
         
     for (var currentQuestion of questions) {
         currentQuestion.addEventListener("onload", calculScore);
@@ -21,21 +23,26 @@ function initialiser(evt) {
 function calculScore(time){
     
     var tempsCourant = this;
+    
     var tempsInitial = 10;
     var scoreMax = 20;
     
-    var pourcent = Math.round((tempsCourant/tempsInitial)*10)/10 ;
+    var pourcent = Math.round((tempsCourant/tempsInitial)*20)/20 ;
     var scoreReponse = pourcent * scoreMax;
+    return scoreReponse;
     
 }
 
 function calculDernierScore(time){
-    var tempsCourant = this;
-    var tempsInitial = 10;
-    var scoreMax = 40;
     
-    var pourcent = Math.round((tempsCourant/tempsInitial)*10)/10 ;
-    var scoreReponse = pourcent * scoreMax;
+    var tempsCourant = this;
+    
+    var tempsInitial = 10;
+    var scoreFinalMax = 40;
+    
+    var pourcent = Math.round((tempsCourant/tempsInitial)*20)/20 ;
+    var scoreDerniereReponse = pourcent * scoreFinalMax;
+    return scoreDerniereReponse;
     
 }
 
