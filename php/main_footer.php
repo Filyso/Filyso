7cab43bed7e80382d71319555cffc7f285504be4
Unknown <clem.hyaumet@gmail.com>
2018-04-23 14:56:33 +0200

    Footer terminé
    footer terminé (manque police à changer éventuellement)

--- php/main_footer.php
+++ php/main_footer.php
@@ -1,3 +1,52 @@
 <?php
 	header("Content-type: text/html; charset: UTF-8");
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+    
+<head>
+    <link rel="stylesheet" href="../styleFooter.css" type="text/css" />
+</head>
+<body>
+    <footer class="footer">
+
+			<div class="footerGauche">
+
+				<img class="logo" src="../images/logo_couleur.png" alt="logo">
+                
+
+				<p class="footer-links">
+					<a href="#">L'équipe</a>
+					·
+					<a href="#">Boîte à idée</a>
+					·
+					<a href="#">À venir</a>
+					
+				</p>
+
+				<div class="footerReseaux">
+
+					<a href="LIEN"><img src="../images/Facebook.png"></a>
+					<a href="LIEN"><img src="../images/Twitter.png"></a>
+					<a href="LIEN"><img src="../images/Instagram.png"></a>
+
+				</div>
+
+			</div>
+
+			<div class="footerDroite">
+
+				<p>Contactez-nous</p>
+
+				<form action="#" method="post">
+
+					<input type="text" name="email" placeholder="Email" />
+					<textarea name="message" placeholder="Message"></textarea>
+					<button>Envoyer</button>
+
+				</form>
+
+			</div>
+
+		</footer>
+</body>
