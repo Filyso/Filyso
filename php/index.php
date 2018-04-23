286edfae99f0656645d9eb4593c94bc9f7836235
Unknown <triclem2@gmail.com>
2018-04-23 11:28:56 +0200

    Organisation page index
    Avec création d'une page select_game.php qui est la page de sélection de mode de jeu.

--- php/index.php
+++ php/index.php
@@ -2,55 +2,33 @@
 	header("Content-type: text/html; charset: UTF-8");
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
+    <!DOCTYPE html>
+    <html lang="fr">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="" content="">
-    <title></title>
-    <meta name="" content="">
-    <link rel="" href="">
+        <meta charset="utf-8">
+        <meta http-equiv="" content="">
+        <title></title>
+        <meta name="" content="">
+        <link rel="" href="">
 
-</head>
+    </head>
 
-<body>
-    <?php include("./main_header.php")?>
-    
+    <body>
+        <?php include("./main_header.php")?>
         <main>
+            <h1>Bienvenue sur Filyso !</h1>
+            <section>
+                <h2>Qu'est-ce que Filsyo ?</h2>
+                <p>Filyso est né dans le cadre d'un projet étudiant en 2018. Il s'agit d'un site vous proposant quelques jeux basés sur les paroles de chansons. Les jeux sont répartis en modes de jeu, en solo ou en multijoueur.</br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac risus quis elit sagittis bibendum tempus a est. Morbi ut nisl convallis, maximus lorem eget, consectetur orci. Nam ornare, lacus vel malesuada tristique, sem nunc facilisis tellus, eu condimentum libero arcu sit amet dui. Mauris sollicitudin nulla ac gravida fringilla. Nunc metus risus, congue ut odio in, bibendum convallis dolor. Aliquam nisi urna, sollicitudin sed faucibus vel, mollis in nisl. Curabitur quis dapibus massa. Etiam condimentum fringilla libero. Quisque non augue eros. Aenean sollicitudin turpis in ipsum lobortis commodo. Cras commodo enim at eros placerat ultricies. Vestibulum commodo euismod placerat. Aliquam a odio feugiat, vehicula est et, convallis odio. </p>
+                
+                <a href="./select_game.php">JOUER</a>
 
-        <section>
-        
-            <div>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            
-            <div>
-                <button></button>
-            </div>
-            
-        </section>
-        
-        <section>
-        
-            <h1></h1>
-            
-            <div>
-            
-                <p></p>
-            
-            </div>
-            
-        </section>
+            </section>
+        </main>
 
-    </main>
+        <?php include("./main_footer.php")?>
+    </body>
 
-    <?php include("./main_footer.php")?>
-</body>
-
-</html>
+    </html>
