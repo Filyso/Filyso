158ccab04353bdbda5f1e35d6878247a41e89c28
Unknown <clem.hyaumet@gmail.com>
2018-04-23 17:01:22 +0200

    HTML Classements
    Pas vraiment finit

---
+++ php/classements.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <title> About </title>
+    <meta name="description" content="À propos de Filyso" />
+</head>
+
+<body>
+    <header>
+    </header>
+    <nav>
+    </nav>
+    <main>
+        <div class="classement">
+            <h1>Top des joueurs</h1>
+            <div class="tableau">
+                
+            </div>
+        </div>
+    </main>
+    <?php include("./main_footer.php") ?>
+</body>
+
+</html>
