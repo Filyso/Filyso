7f1000dd455b5fde6e6ff657d4d04730d0944895
Unknown <triclem2@gmail.com>
2018-04-23 14:46:51 +0200

    Orga page index et CSS index fini

--- php/index.php
+++ php/index.php
@@ -12,7 +12,6 @@
         <title></title>
         <meta name="" content="">
         <link rel="stylesheet" href="../index.css">
-
     </head>
 
     <body>
@@ -21,10 +20,10 @@
             <h1>Bienvenue sur Filyso !</h1>
             <section>
                 <h2>Qu'est-ce que Filsyo ?</h2>
-                <p>Filyso est né dans le cadre d'un projet étudiant en 2018. Il s'agit d'un site vous proposant quelques jeux basés sur les paroles de chansons. Les jeux sont répartis en modes de jeu, en solo ou en multijoueur.</br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac risus quis elit sagittis bibendum tempus a est. Morbi ut nisl convallis, maximus lorem eget, consectetur orci. Nam ornare, lacus vel malesuada tristique, sem nunc facilisis tellus, eu condimentum libero arcu sit amet dui. Mauris sollicitudin nulla ac gravida fringilla. Nunc metus risus, congue ut odio in, bibendum convallis dolor. Aliquam nisi urna, sollicitudin sed faucibus vel, mollis in nisl. Curabitur quis dapibus massa. Etiam condimentum fringilla libero. Quisque non augue eros. Aenean sollicitudin turpis in ipsum lobortis commodo. Cras commodo enim at eros placerat ultricies. Vestibulum commodo euismod placerat. Aliquam a odio feugiat, vehicula est et, convallis odio. </p>
-
-                <a href="./select_game.php">JOUER</a>
+                <p>Filyso est né dans le cadre d'un projet étudiant en 2018. Il s'agit d'un site vous proposant quelques jeux basés sur les paroles de chansons. Les jeux sont répartis en modes de jeu, en solo ou en multijoueur.</br>
+                Notre application est développée au département MMI de l'IUT de Laval, en France. Le but est d'imaginer et de concevoir une application en rapport avec un certain sujet et présentant certaines fonctionnalités. La première version que nous vous présentons ici est un prototype dans lequel seul le mode de jeu en solo n'est disponible.</p>
             </section>
+                <a href="./select_game.php">JOUER</a>
         </main>
 
         <?php include("./main_footer.php")?>
