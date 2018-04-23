5789713864ecdc18a56d5e03183fa6902dd157a4
Unknown <yoyan.bid@gmail.com>
2018-04-23 15:03:29 +0200

    MAJ CSS

--- php/main_header.php
+++ php/main_header.php
@@ -3,7 +3,7 @@
 ?>
 
     <head>
-        <link rel="stylesheet" href="../header.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="../style.css" type="text/css" media="screen" />
     </head>
 
     <body>
