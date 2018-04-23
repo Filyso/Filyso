e1893f2a79e02396213dbdb7ccd6980130b58911
Unknown <villatte.maxime@orange.fr>
2018-04-23 16:26:21 +0200

    JStimer terminer

--- javascript/main.js
+++ javascript/main.js
@@ -2,14 +2,27 @@
 
 document.addEventListener("DOMContentLoaded",initialiser);
 
+var timerMilli;
+var timerSec;
+
 function initialiser(evt){
 
     
-    
+    window.setInterval(increment,10);
     
 }
 
 
-function """"""(evt){
-
+function increment(evt){
+    timerSec = 0;
+    timerMilli = 0;
+    
+    timerMilli = timerMilli + 1;
+    
+    if(timerMilli >= 100){
+        timerMilli = 0;
+        timerSec = timerSec + 1;
+    }
+    
+    
 }
