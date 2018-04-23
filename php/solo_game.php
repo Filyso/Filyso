940e06dab79cc2f7df8f28e268d50c1080f01923
Unknown <justineagnelet@gmail.com>
2018-04-23 11:52:02 +0200

    Pause Midi

--- php/solo_game.php
+++ php/solo_game.php
@@ -10,38 +10,52 @@
     <meta charset="utf-8">
     <title>Jeu en Solo</title>
     <meta name="description" content="Jouez !">
-    <link rel="stylesheet" type="text/css" href="style_jeu_solo.css">
+    <link rel="stylesheet" type="text/css" href="../style_jeu_solo.css">
 
 </head>
 
 <body>
-    <?php include("./main_header.php")?>
+    <?php include("./main_header.php");?>
     
     <main>
 
         <section>
             
-            <div id="photoProfil" class="photoProfil"></div>
+            <div>
             
-            <progress id="score" class="score" value="0" max="100">Votre score</progress>
+                <div id="photoProfil" class="photoProfil">
+                    <img alt="Photo de profil" src="../images/chat.jpg"/>
+                </div>
+            
+                <progress id="score" class="score" value="20" max="100">Votre score</progress>
+            
+            </div>
+            
+            <div>
 
-            <input type="button" value="?" id="tutoGameButton" class="tutoButton">
+                <input type="button" value="?" id="tutoGameButton" class="tutoButton">
             
-            <p id="numQuestion" class="numQuestion">N°1</p>
+                <p id="numQuestion" class="numQuestion">N°1</p>
             
-            <p id="phraseACompleter" class="phraseACompleter">Phrase à compléter</p>
+                <p id="phraseACompleter" class="phraseACompleter">Phrase à compléter</p>
+                
+            </div>
             
-            <input type="button" value="Réponse n°1" id="reponse1Button" class="reponse1Button">
+            <div class="reponse">
+            
+                <input type="button" value="Réponse n°1" id="reponse1Button" class="reponse1Button">
+            
+                <input type="button" value="Réponse n°2" id="reponse2Button" class="reponse2Button">
+                
+            </div>
             
             <div id="timer" class="timer"></div>
-            
-            <input type="button" value="Réponse n°2" id="reponse2Button" class="reponse2Button">
             
         </section>
 
     </main>
 
-    <?php include("./main_footer.php")?>
+    <?php include("./main_footer.php");?>
 </body>
 
 </html>
