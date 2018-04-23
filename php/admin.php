dec9b238e7d44305cd7fec286dac96af8e23815c
Unknown <vscavinner@gmail.com>
2018-04-23 15:27:04 +0200

    HTMLadmin v1
    Code html basique avec les deux include (de l'en-tête et du footer)

--- php/admin.php
+++ php/admin.php
@@ -1,3 +1,45 @@
 <?php
 	header("Content-type: text/html; charset: UTF-8");
 ?>
+<html>
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <title>Choix des options de jeu</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+
+    </head>
+    <body>
+        
+        <?php //include("./main_header.php"); ?>
+        
+        <section>
+            
+            <div>
+    
+                <input type="button" value="Gestion de contenu" disabled="disabled"/>
+                <input type="button" value="Ajout de médias"/>
+                <input type="button" value="Modification/Suppression de médias"/>
+                
+            </div>
+            
+            <div>
+              
+                <input type="button" value="Gestion utilisateurs"/>
+                
+            </div>
+    
+        </section>
+        
+        <section>
+            
+            
+    
+        </section>
+        
+        <?php //include("./main_footer.php"); ?>
+        
+    </body>
+    
+</html>
