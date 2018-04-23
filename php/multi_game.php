d5daeb6127ae120711c984e076b778e09c51c24b
Unknown <triclem2@gmail.com>
2018-04-23 17:10:18 +0200

    CSS page choix langue

---
+++ php/multi_game.php
@@ -0,0 +1,25 @@
+<?php
+	header("Content-type: text/html; charset: UTF-8");
+?>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="" content="">
+        <title></title>
+        <meta name="" content="">
+        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="../style_kraken.css">
+    </head>
+
+    <body>
+        <?php include("./main_header.php")?>
+        <main>
+            <h1>Le jeu en multijoueur est encore en développement !</h1>
+        </main>
+
+        <?php include("./main_footer.php")?>
+    </body>
+
+    </html>
