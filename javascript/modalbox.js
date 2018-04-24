5f3f81c278ffa73af6453a691bcd3a06d5b10b13
Unknown <vscavinner@gmail.com>
2018-04-24 09:05:33 +0200

    CSS_JS_ModalBox V3

--- javascript/modalbox.js
+++ javascript/modalbox.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", initialiser);
 
+var modal;
 
 function initialiser(evt) {
+    modal = document.getElementById("myModal");
+    
     
     var btn = document.getElementById("myBtn");
-    myBtn.addEventListener("onclick", faireApparaitre);
-
-    var modal = document.getElementById("myModal");
-
+    myBtn.addEventListener("click", faireApparaitre);
     
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close");
+    span.addEventListener("click", faireDisparaitre);
 
 }
 
 function faireApparaitre (evt) {
-    console.log(modal);
     modal.style.display = "block";
 }
 
@@ -23,9 +23,9 @@ function faireDisparaitre (evt) {
     modal.style.display = "none";
 }
 
-
+/*
 window.onclick = function () {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+}*/
