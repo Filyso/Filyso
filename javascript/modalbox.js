d94ee2ab8aacec9ea6725e2c11729efadaa98682
Unknown <vscavinner@gmail.com>
2018-04-24 12:00:36 +0200

    CSS_JS_ModalBox V4
    Ajout du "onload" + mise en commentaire de l'animation

--- javascript/modalbox.js
+++ javascript/modalbox.js
@@ -3,29 +3,42 @@ document.addEventListener("DOMContentLoaded", initialiser);
 var modal;
 
 function initialiser(evt) {
+
     modal = document.getElementById("myModal");
-    
-    
+
     var btn = document.getElementById("myBtn");
-    myBtn.addEventListener("click", faireApparaitre);
-    
+    btn.addEventListener("click", faireApparaitre);
+
     var span = document.getElementsByClassName("close");
-    span.addEventListener("click", faireDisparaitre);
+    span[0].addEventListener("click", faireDisparaitre);
 
 }
+
 
 function faireApparaitre (evt) {
     modal.style.display = "block";
 }
 
+/*function faireApparaitre(evt) {
+    console.log("test");
+
+    if (modal.classList.contains("is-paused")) {
+        modal.classList.remove("is-paused");
+    }
+}*/
 
 function faireDisparaitre (evt) {
     modal.style.display = "none";
 }
 
-/*
+
 window.onclick = function () {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}*/
+}
+
+window.onload = function () {
+    modal.style.display = "block";
+}
+
