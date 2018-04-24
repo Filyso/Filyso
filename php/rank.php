0d3a95a5ad3f937a839219157125a35754385e9a
Tristou <triclem2@gmail.com>
2018-04-24 14:46:54 +0200

    save2 css admin

--- php/rank.php
+++ php/rank.php
@@ -1,3 +1,71 @@
 <?php
 	header("Content-type: text/html; charset: UTF-8");
 ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="" content="">
+        <title></title>
+        <meta name="" content="">
+        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="../style_rank.css">
+    </head>
+
+    <body>
+        <?php include("./main_header.php")?>
+        <main class="mainRank">
+            <h1>Classement des joueurs</h1>
+            <table>
+                <tr>
+                    <th>Joueur</th>
+                    <th>Classement</th>
+                </tr>
+                <tr>
+                    <td>Michel53</td>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <td>LopezDavid</td>
+                    <td>2</td>
+                </tr>
+                <tr>
+                    <td>SergeGB</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>Philippe</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>Seb</td>
+                    <td>5</td>
+                </tr>
+                <tr>
+                    <td>julBrass</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>FrancisL</td>
+                    <td>7</td>
+                </tr>
+                <tr>
+                    <td>xXManchotXx</td>
+                    <td>8</td>
+                </tr>
+                <tr>
+                    <td>TheBernard</td>
+                    <td>9</td>
+                </tr>
+                <tr>
+                    <td>SchmoutC</td>
+                    <td>10</td>
+                </tr>
+            </table>
+        </main>
+        <?php include("./main_footer.php")?>
+    </body>
+
+    </html>
