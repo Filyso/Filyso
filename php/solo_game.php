5e2760ee9aa288f6770a101907f22f3959b52dce
Unknown <vscavinner@gmail.com>
2018-04-25 09:03:13 +0200

    PHP_recupererDonnees V2
    Détermination de la requête à effectuer (fin étape 2)

--- php/solo_game.php
+++ php/solo_game.php
@@ -7,78 +7,78 @@
             $pdo->query("SET NAMES utf8");
             $pdo->query("SET CHARACTER SET 'utf8'");
     // ETAPE 2 : Envoyer une requête SQL
-            $requeteSQL = "SELECT CHANSONS WHERE lang=".$_GET["langue"];
-            $requeteSQL = ""
-
+            $requeteSQL = "SELECT CHANSONS.lang , CATEGORIES.idCat FROM CHANSONS INNER JOIN APPARTIENT_A_UNE ON CHANSONS.idSong = APPARTIENT_A_UNE.idSong INNER JOIN CATEGORIES
+ON APPARTIENT_A_UNE.idCat = CATEGORIES.idCat WHERE lang =".$_GET["langue"]" and idCat =".$_GET["categorie"];
+    // ETAPE 3 :
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
+    <!DOCTYPE html>
+    <html lang="fr">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <title>Jeu en Solo</title>
-    <meta name="description" content="Jouez !">
-    <link rel="stylesheet" type="text/css" href="../style.css"/>
+        <meta charset="utf-8">
+        <title>Jeu en Solo</title>
+        <meta name="description" content="Jouez !">
+        <link rel="stylesheet" type="text/css" href="../style.css" />
 
-</head>
+    </head>
 
-<body>
-    <?php include("./main_header.php");?>
-    <main>
+    <body>
+        <?php include("./main_header.php");?>
+            <main>
 
-        <section>
-            
-            <div class="score">
-            
-                <figure>
-                    <img alt="Photo de profil" src="../images/chat.jpg"/>
-                    
-                </figure>
-                
-                <div class="barScoreMax">
-                    <div class="barScore">
-                    </div>
-                </div>
-            
-            </div>
-            
-            <div class="contenu">
-                
-                <div class="numEtTuto">
+                <section>
 
-                    <input type="button" value="?" id="tutoButton" class="tutoButton"/>
-            
-                    <p id="numQuestion" class="numQuestion">Question n°1</p>
-                    
-                </div>
-            
-                <p id="phraseACompleter" class="phraseACompleter">Phrase à compléter</p>
-                
-                <div class="reponses">
-                    <div class="Sousreponses">
-                        <input type="button" value="Réponse n°1" id="reponse1Button" class="reponseButton"/>
+                    <div class="score">
 
-                        <input type="button" value="Réponse n°2" id="reponse2Button" class="reponseButton"/>
-                    </div>
-                        <div class="divTimer">
-                            <p id="timer" class="timer">00</p>
+                        <figure>
+                            <img alt="Photo de profil" src="../images/chat.jpg" />
+
+                        </figure>
+
+                        <div class="barScoreMax">
+                            <div class="barScore">
+                            </div>
                         </div>
-                    <div class="Sousreponses">
-                        <input type="button" value="Réponse n°3" id="reponse3Button" class="reponseButton"/>
 
-                        <input type="button" value="Réponse n°4" id="reponse4Button" class="reponseButton"/>
                     </div>
-                </div>
-                
-            </div>
-            
-        </section>
 
-    </main>
+                    <div class="contenu">
 
-    <?php //include("./main_footer.php");?>
-</body>
+                        <div class="numEtTuto">
 
-</html>
+                            <input type="button" value="?" id="tutoButton" class="tutoButton" />
+
+                            <p id="numQuestion" class="numQuestion">Question n°1</p>
+
+                        </div>
+
+                        <p id="phraseACompleter" class="phraseACompleter">Phrase à compléter</p>
+
+                        <div class="reponses">
+                            <div class="Sousreponses">
+                                <input type="button" value="Réponse n°1" id="reponse1Button" class="reponseButton" />
+
+                                <input type="button" value="Réponse n°2" id="reponse2Button" class="reponseButton" />
+                            </div>
+                            <div class="divTimer">
+                                <p id="timer" class="timer">00</p>
+                            </div>
+                            <div class="Sousreponses">
+                                <input type="button" value="Réponse n°3" id="reponse3Button" class="reponseButton" />
+
+                                <input type="button" value="Réponse n°4" id="reponse4Button" class="reponseButton" />
+                            </div>
+                        </div>
+
+                    </div>
+
+                </section>
+
+            </main>
+
+            <?php //include("./main_footer.php");?>
+    </body>
+
+    </html>
