2d15b115d7a062abfd6f233bf5a8d7e9da3ae801
Tristou <triclem2@gmail.com>
2018-04-25 17:29:06 +0200

    machin qui me saoule save 3

--- javascript/valid_time_code.js
+++ javascript/valid_time_code.js
@@ -3,18 +3,18 @@
 
     document.addEventListener("DOMContentLoaded", initialiser);
 
-    function initialiser(evt) {
-        var timecode1 = document.getElementById("minStart_1");
+    var timecode1 = document.getElementById("minStart_1");
+    var timecode2 = document.getElementById("minEnd_2");
 
+    function initialiser(evt) {
         timecode1.addEventListener("change", verifier);
+        timecode2.addEventListener("change", verfier);
     }
 
     function verifier(evt) {
-        var timecode2 = document.getElementById("minEnd_2");
 
         this.setCustomValidity("");
-        window.alert(this.value);
-        if (this.value >= timecode2.value) {
+        if (timecode1.value >= timecode2.value) {
             this.setCustomValidity("Le temps du début doit être inférieur au temps de fin.");
         }
     }
